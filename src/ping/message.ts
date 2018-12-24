59a6fa1771a8f399365daf41ea9164af1e270305
Entwickler7aSv <entwickler7asv@yahoo.com>
2018-12-24 22:54:04 +0300

    Removing issues #3

--- src/ping/message.ts
+++ src/ping/message.ts
@@ -1,6 +1,6 @@
 import { DisEvent } from "@disnetwork/core";
 
-export class PingMessageEvent extends DisEvent{
+export class PingMessageEvent extends DisEvent {
 
     public onFire(message: any) {
         // Fire when any message sent to the bot
